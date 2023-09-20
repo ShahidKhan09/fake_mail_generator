ac5f269c1034fd253350366a9c305ea0c8dcbe94
Muhammad Shahid <69686858+ShahidKhan09@users.noreply.github.com>
2023-09-20 11:05:29 +0500

    dMail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
         <Box width="100%" marginTop="auto" bottom={0} paddingY={3} boxShadow={4} component="footer" aria-label="footer">
             <Container component="nav">
                 <Stack direction="row" justifyContent="space-between" alignItems="center">
-                    <Typography aria-label="credits">dMail - Designed and created by Joseph Cano</Typography>
-                    <IconButton aria-label="github repository" href="https://github.com/Jcanotorr06/dMail" rel="noreferrer" target="_blank">
+                    <Typography aria-label="credits">dMail - Designed and created by shahid</Typography>
+                    {/* <IconButton aria-label="github repository" href="https://github.com/Jcanotorr06/dMail" rel="noreferrer" target="_blank">
                         <GitHub/>
-                    </IconButton>
+                    </IconButton> */}
                 </Stack>
             </Container>
         </Box>
